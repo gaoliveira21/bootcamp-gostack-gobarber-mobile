a975a43853ef33a5b6e30ca2c7363069226a73cf
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-07-01 15:10:31 -0300

    Background, Input and Button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 const SignIn = () => {
-  return <Text>Sign In</Text>;
+  return (
+    <Background>
+      <Input icon="call" placeholder="Digite seu nome" />
+      <Button>Entrar</Button>
+    </Background>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 const SignUp = () => {
-  return <Text>Sign Up</Text>;
+  return (
+    <Background>
+      <Text>Sign Up</Text>
+    </Background>
+  );
 };
 
 export default SignUp;
