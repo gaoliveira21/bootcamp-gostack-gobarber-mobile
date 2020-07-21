986895ac8dd6190e46dc139ee8ec0a2e020809e1
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-07-21 15:09:22 -0300

    list appointments and cancel them

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-const Appointment = () => {
+const Appointment = ({ data, onCancel }) => {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
           source={{
-            uri: 'https://api.adorable.io/avatars/50/abott@adorable.png',
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatars/50/${data.provider.name}.png`,
           }}
         />
         <Info>
-          <Name>Gabriel</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
-
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
+};
+
+Appointment.propTypes = {
+  data: PropTypes.shape({
+    canceled_at: PropTypes.string,
+    past: PropTypes.bool,
+    date: PropTypes.string,
+    cancelable: PropTypes.bool,
+    provider: PropTypes.shape({
+      name: PropTypes.string,
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
 };
 
 export default Appointment;
